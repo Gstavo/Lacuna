42da399f432f51e10f616792d9f5fdc334977cbf
SwathingBlock <tiagocf_13@hotmail.com>
2016-06-24 02:22:08 +0100

    boas

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -3,96 +3,102 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float walkSpeed = 1;
-    private bool isGrounded = true;
+	public float walkSpeed = 1;
+	private bool isGrounded = true;
+	private string anim = "Idle";
+	bool isWalking = false;
 
-    bool isWalking = false;
+	Animator animator;
+	//animation states - the values in the animator conditions
+	const int STATE_IDLE = 0;
+	const int STATE_WALK = 1;
 
-    Animator animator;
-    //animation states - the values in the animator conditions
-    const int STATE_IDLE = 0;
-    const int STATE_WALK = 1;
+	int currentAnimationState = STATE_IDLE;
+	string currentDirection = "left";
 
-    int currentAnimationState = STATE_IDLE;
-    string currentDirection = "left";
- 
 
-    // Use this for initialization
-    void Start () {
-        animator = this.GetComponent<Animator>();
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		
+
 	}
 
-    void FixedUpdate() {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
+	void FixedUpdate() {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
 
-        if (Input.GetKey("up") && isGrounded) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250));
-            //animator.SetInteger("state", STATE_JUMP);
-            isGrounded = false;
-        }
-        else if (Input.GetKey("right"))
-        {
-            changeDirection("right");
-            transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+		if (Input.GetKey("up") && isGrounded) {
+			animator.SetInteger("state", STATE_IDLE);
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250));
+			//animator.SetInteger("state", STATE_JUMP);
+			isGrounded = false;
+		}
+		else if (Input.GetKey("right"))
+		{
+			animator.SetInteger("state", STATE_WALK);
+			changeDirection("right");
+			transform.Translate(Vector3.right * walkSpeed * Time.deltaTime);
 
-            if (this.currentAnimationState != STATE_WALK) {
-                animator.SetInteger("state", STATE_IDLE);
-                currentAnimationState = STATE_IDLE;
-            }
-        }
-        else if (Input.GetKey("left"))
-        {
-            changeDirection("left");
-            transform.Translate(Vector3.right * walkSpeed * Time.deltaTime);
+			if (this.currentAnimationState != STATE_WALK) {
+				
+				currentAnimationState = STATE_WALK;
+			}
+		}
+		else if (Input.GetKey("left"))
+		{
+			
+			animator.SetInteger("state", STATE_WALK);
 
-            if (this.currentAnimationState != STATE_IDLE)
-            {
-                animator.SetInteger("state", STATE_IDLE);
-                currentAnimationState = STATE_IDLE;
-            }
-        }
+			changeDirection("left");
+			transform.Translate(Vector3.right * walkSpeed * Time.deltaTime);
 
-        //check if walk animation is playing
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk"))
-            isWalking = true;
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
-            isWalking = false;
+			if (this.currentAnimationState != STATE_IDLE)
+			{
+				
+				currentAnimationState = STATE_WALK;
+			}
+		}
 
-    }
+		//check if walk animation is playing
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("walk"))
+			isWalking = true;
+		else if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+			isWalking = false;
 
-    // Check if player has collided with the floor
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.name == "Floor")
-        {
-            isGrounded = true;
-            animator.SetInteger("state", STATE_IDLE);
-        }
+	}
 
-    }
+	// Check if player has collided with the floor
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.name == "Floor")
+		{
+			isGrounded = true;
+			animator.SetInteger("state", STATE_IDLE);
+		}
 
-    void changeDirection(string direction)
-    {
+	}
 
-        if (currentDirection != direction)
-        {
-            if (direction == "right")
-            {
-                transform.Rotate(0, 180, 0);
-                currentDirection = "right";
-            }
-            else if (direction == "left")
-            {
-                transform.Rotate(0, -180, 0);
-                currentDirection = "left";
-            }
-        }
+	void changeDirection(string direction)
+	{
 
-    }
+		if (currentDirection != direction)
+		{
+			if (direction == "right")
+			{
+				transform.Rotate(0, 180, 0);
+				currentDirection = "right";
+			}
+			else if (direction == "left")
+			{
+				transform.Rotate(0, -180, 0);
+				currentDirection = "left";
+			}
+		}
+
+	}
 }
